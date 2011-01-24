1c06b5b8ed8e6dc8fe1c7932381246db5f732166
Erik Blaufuss <blaufuss@icecube.umd.edu>
2011-01-24 01:56:09 +0000

    Bring goodness from the trunk into the RC
    git-svn-id: http://code.icecube.wisc.edu/icetray/projects/cmake/releases/11-01-00@572 579d543e-85e3-4578-a5e8-1e6a993b7602

--- tools.cmake
+++ tools.cmake
@@ -41,8 +41,8 @@ endif()
 #  These guys are in subdirectory 'tools'
 #
 set(ALL_TOOLS pthread root boost python hippodraw
-    rdmc expat gsl sprng sla libarchive
-    mysql bdb log4cplus photonics ptd
+    rdmc expat gsl sprng sla libarchive blas
+    mysql bdb log4cplus photonics ptd suitesparse
     ZThread omniORB fox jni ncurses
     cdk qt4 cfitsio hdf5 minuit2 clhep geant4
     ${I3_EXTRA_TOOLS}   # add the extra tools and dedupe
---
+++ tools/suitesparse.cmake
@@ -0,0 +1,8 @@
+
+TOOLDEF (suitesparse
+  include
+  suitesparse/cholmod.h
+  lib
+  NONE
+  camd ccolamd spqr cholmod amd colamd
+  )
