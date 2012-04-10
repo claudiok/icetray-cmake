61f791945c0e82c1bdb03a2ea69152533f31fbf9
Don LaDieu <nega@icecube.umd.edu>
2012-04-10 21:02:07 +0000

    new libarchive detection
    libarchive detection wasn't working on some of the bots
    
    
    git-svn-id: http://code.icecube.wisc.edu/icetray/projects/cmake/branches/ctest@1188 579d543e-85e3-4578-a5e8-1e6a993b7602

--- tools/libarchive.cmake
+++ tools/libarchive.cmake
@@ -1,31 +1,33 @@
-colormsg("")
-colormsg(HICYAN "libarchive")
+tooldef(libarchive
+  include
+  archive.h
+  lib
+  NONE
+  archive
+)
 
-find_package(PkgConfig)
-if(${CMAKE_VERSION} VERSION_LESS "2.8.2")
-  pkg_check_modules(PC_LA libarchive)
-else(${CMAKE_VERSION} VERSION_LESS "2.8.2")
-  pkg_check_modules(PC_LA QUIET libarchive)
-endif(${CMAKE_VERSION} VERSION_LESS "2.8.2")
+if(LIBARCHIVE_FOUND)
+  set(LIBARCHIVEINI3PORTS_FOUND TRUE)
+else()
+  unset(LIBARCHIVE_CONFIG_ERROR)
+  unset(LIBARCHIVE_INCLUDE_DIR)
+  #colormsg(YELLOW "*** libarchive not found in \$I3_PORTS")
+  #colormsg(YELLOW "*** Searching for 3rd party libarchive")
 
-find_library(LIBARCHIVE_LIBRARIES archive
-             HINTS ${I3_PORTS}/lib
-                   ${PC_LA_LIBDIR}
-                   ${PC_LA_LIBRARY_DIRS}
-                   ${TOOL_SYSTEM_PATH})
+  find_package(LibArchive QUIET)
 
-find_path(LIBARCHIVE_INCLUDE_DIR archive.h
-          HINTS ${I3_PORTS}/include
-                ${PC_LA_INCLUDEDIR}
-                ${PC_LA_INCLUDE_DIRS})
+  if(LibArchive_INCLUDE_DIRS AND LibArchive_LIBRARIES)
+    set(LIBARCHIVE_INCLUDE_DIR ${LibArchive_INCLUDE_DIRS})
+    set(LIBARCHIVE_LIBRARIES ${LibArchive_LIBRARIES})
+    set(LIBARCHIVE_FOUND TRUE CACHE BOOL "")
+  endif(LibArchive_INCLUDE_DIRS AND LibArchive_LIBRARIES)
 
-if(NOT ${LIBARCHIVE_INCLUDE_DIR} MATCHES "-NOTFOUND")
-  found_ok("archive.h found at ${LIBARCHIVE_INCLUDE_DIR}")
-endif(NOT ${LIBARCHIVE_INCLUDE_DIR} MATCHES "-NOTFOUND")
-if(NOT ${LIBARCHIVE_LIBRARIES} MATCHES "-NOTFOUND")
-  found_ok(${LIBARCHIVE_LIBRARIES})
-endif(NOT ${LIBARCHIVE_LIBRARIES} MATCHES "-NOTFOUND")
+  if(NOT ${LIBARCHIVE_INCLUDE_DIR} MATCHES "-NOTFOUND")
+    found_ok("archive.h found at ${LIBARCHIVE_INCLUDE_DIR}")
+  endif(NOT ${LIBARCHIVE_INCLUDE_DIR} MATCHES "-NOTFOUND")
 
-if(LIBARCHIVE_INCLUDE_DIR AND LIBARCHIVE_LIBRARIES)
-  set(LIBARCHIVE_FOUND TRUE CACHE BOOL "")
-endif(LIBARCHIVE_INCLUDE_DIR AND LIBARCHIVE_LIBRARIES)
+  if(NOT ${LIBARCHIVE_LIBRARIES} MATCHES "-NOTFOUND")
+    found_ok(${LIBARCHIVE_LIBRARIES})
+  endif(NOT ${LIBARCHIVE_LIBRARIES} MATCHES "-NOTFOUND")
+
+endif()
