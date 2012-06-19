42a6728792afba9ce1a84596a02ddec4d667a2eb
Alex Olivas <olivas@icecube.umd.edu>
2012-06-19 09:41:06 +0000

    i second that.  please, for the love of god, stop binutils stupidity.
    git-svn-id: http://code.icecube.wisc.edu/icetray/projects/cmake/branches/V11-08-00-sim@1265 579d543e-85e3-4578-a5e8-1e6a993b7602

--- config.cmake
+++ config.cmake
@@ -439,6 +439,15 @@ if(NOT METAPROJECT_CONFIGURED)
   set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO}" CACHE STRING
     "Flags used by compiler during release builds" FORCE)
 
+  #
+  # stop binutils stupidity
+  #
+  if(NOT APPLE)
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-as-needed" CACHE STRING "Flags used by the linker" FORCE)
+    set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -Wl,--no-as-needed" CACHE STRING "Flags used by the linker" FORCE)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--no-as-needed" CACHE STRING "Flags used by the linker" FORCE)
+  endif(NOT APPLE)
+
   set(METAPROJECT_CONFIGURED TRUE CACHE INTERNAL "Metaproject configured")
 endif(NOT METAPROJECT_CONFIGURED)
 
