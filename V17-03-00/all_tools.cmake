8023612c0a35ba4de12a89895c9793e932f1608a
Alex Olivas <olivas@icecube.umd.edu>
2017-03-31 22:36:07 +0000

    making an incremental release
    git-svn-id: http://code.icecube.wisc.edu/icetray/projects/cmake/releases/V17-03-01@2842 579d543e-85e3-4578-a5e8-1e6a993b7602

---
+++ V17-03-00/all_tools.cmake
@@ -0,0 +1,12 @@
+#
+#  These guys are in subdirectory 'tools'
+#
+set(ALL_TOOLS pthread root python boost boostnumpy
+    blas lapack gsl sprng sla libarchive
+    mysql mpi suitesparse ncurses cdk
+    cfitsio healpix qt4 hdf5 minuit2 clhep geant4 zlib
+    opencl gmp log4cpp xml2 genie zmq doxygen
+    multinest nlopt hepmc
+    ${I3_EXTRA_TOOLS}   # add the extra tools and dedupe
+    )
+list(REMOVE_DUPLICATES ALL_TOOLS)
